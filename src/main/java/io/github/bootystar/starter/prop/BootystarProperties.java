037cd87ca50f40047b14e645cbc65bb41d495cf3
bootystar <bootystar@qq.com>
2025-01-08 10:08:33 +0800

    -.

--- src/main/java/io/github/bootystar/starter/prop/BootystarProperties.java
+++ src/main/java/io/github/bootystar/starter/prop/BootystarProperties.java
@@ -6,8 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 /**
- *
- *
  * @author bootystar
  */
 @Data
