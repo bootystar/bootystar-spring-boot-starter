e468c76f848ec1b396722971ed9e2f08c5b817a8
bootystar <bootystar@qq.com>
2025-01-08 23:49:27 +0800

    .

--- src/main/java/io/github/bootystar/starter/autoconfigure/suppoert/MybatisPlusConfiguration.java
+++ src/main/java/io/github/bootystar/starter/autoconfigure/suppoert/MybatisPlusConfiguration.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.InnerInterceptor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
